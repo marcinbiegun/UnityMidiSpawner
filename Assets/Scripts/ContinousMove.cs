580a5cabff1df49f03ac12e22e5917304ff11ad2
Marcin Biegun <marcin@mm.st>
2018-05-22 18:47:47 +0200

    Add scripts from old project

---
+++ Assets/Scripts/ContinousMove.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContinousMove : MonoBehaviour {
+
+    public Vector3 velocity;
+
+	void Update () {
+        transform.Translate(velocity * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject spawnPrefab;
+    public GameObject spawnsHolder;
+
+    public float spawnThurst = 1f;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+    void OnEnable() {
+        Vector3 randomDirection = Random.insideUnitSphere;
+        Quaternion randomRotation = Random.rotation;
+
+        GameObject newObject = Instantiate(spawnPrefab, transform.position, randomRotation);
+        newObject.transform.SetParent(spawnsHolder.transform);
+
+        Rigidbody rb = newObject.GetComponent<Rigidbody>();
+        rb.AddForce(randomDirection * spawnThurst);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
