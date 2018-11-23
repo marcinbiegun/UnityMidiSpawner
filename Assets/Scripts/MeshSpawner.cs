b7498daf35ae8283b7f4519b9ea252ea41740609
Marcin Biegun <marcin@mm.st>
2018-11-24 00:24:04 +0100

    Draft for mesh spawner script

---
+++ Assets/Scripts/MeshSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class MeshSpawner : MonoBehaviour {
+
+    Mesh mesh;
+    Vector3[] vertices;
+    int[] triangles;
+
+	void Awake() {
+        mesh = GetComponent<MeshFilter>().mesh;
+	}
+
+    void Start() {
+        MakeMeshData();
+        UpdateMesh();
+    }
+
+    void MakeMeshData() {
+        vertices = new Vector3[] {
+            new Vector3(0,0,0),
+            new Vector3(0,0,1),
+            new Vector3(1,0,0)
+        };
+
+        triangles = new int[] { 0, 1, 2 };
+    }
+
+    void UpdateMesh() {
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+    }
+
+}
+
